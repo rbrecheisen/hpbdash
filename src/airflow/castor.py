7d304fc5c1bb9722219953058b55c0441be5947e
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2022-11-29 16:08:25 +0100

    Saving work

--- src/airflow/castor.py
+++ src/airflow/castor.py
@@ -1,13 +1,22 @@
+import os
+
+from barbell2.castor.api import CastorApiClient
 from datetime import datetime
 from airflow.decorators import dag, task
+
+CLIENT_ID = os.environ['CASTOR_CLIENT_ID']
+CLIENT_SECRET = os.environ['CASTOR_CLIENT_SECRET']
 
 
 @dag(schedule=None, start_date=datetime.now())
 def castor():
-    @task(task_id='connect')
-    def connect(ds=None, **kwargs):
-        print('Executing task connect...')
-    connect()
+
+    @task(task_id='extract_data')
+    def extract_data(ds=None, **kwargs):
+        print(CLIENT_ID)
+        print(CLIENT_SECRET)
+
+    extract_data()
 
 
 castor()
