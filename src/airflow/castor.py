b25ddbfa23ea6ad7b7c50090dd0065d8453b7d46
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2022-11-29 14:54:50 +0100

    Saving work

--- src/airflow/castor.py
+++ src/airflow/castor.py
@@ -1,7 +1,8 @@
+from datetime import datetime
 from airflow.decorators import dag, task
 
 
-@dag(schedule=None)
+@dag(schedule=None, start_date=datetime.now())
 def castor():
     @task(task_id='connect')
     def connect(ds=None, **kwargs):
