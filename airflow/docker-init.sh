d4933921abcc56148390c5a62ae4de5e259c05a1
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2022-11-29 14:43:23 +0100

    Saving work

--- airflow/docker-init.sh
+++ airflow/docker-init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p ./dags ./logs ./plugins
-echo -e "AIRFLOW_UID=$(id -u)" > .env
+echo -e "AIRFLOW_UID=$(id -u)" > .env  # IMPORTANT! Without this environment variable, it doesn't work!
 docker-compose up airflow-init
--- airflow/docker-run.sh
+++ airflow/docker-run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+cp ../src/airflow/* ./dags
 docker-compose up -d --force-recreate && docker-compose logs -f
