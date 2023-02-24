f0ad933d87656c0ffa352edda3e5220d5856f592
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2023-02-24 09:21:56 +0100

    Saving work

--- src/airflow/castor2sqlite.py
+++ src/airflow/castor2sqlite.py
@@ -2,7 +2,6 @@ import os
 import logging
 import pendulum
 
-from datetime import datetime, timedelta
 from airflow.decorators import dag, task
 from airflow.operators.python import get_current_context
 from barbell2.castor.castor2sqlite import CastorToSqlite
@@ -17,7 +16,7 @@ LOGGER = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-@dag(schedule='*/2 * * * *', start_date=pendulum.now(TIMEZONE).subtract(minutes=1), catchup=False)
+@dag(schedule='*/2 * * * *', start_date=pendulum.now().subtract(minutes=1), catchup=False)
 def castor2sqlite():
 
     @task(task_id='extract_data')
@@ -38,7 +37,7 @@ def castor2sqlite():
 
     @task(task_id='save_file')
     def save_file():
-        timestamp = pendulum.now(TIMEZONE).strftime('%m-%d-%Y_%H:%M:%S')
+        timestamp = pendulum.now().strftime('%m-%d-%Y_%H:%M:%S')
         with open('/tmp/{}.txt'.format(timestamp), 'w') as f:
             f.write('hello!')
 
