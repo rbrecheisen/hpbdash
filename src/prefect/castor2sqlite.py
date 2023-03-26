38b618921bbb28dc84d7324496f1ad5a925dd0e0
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2023-03-26 11:12:24 +0200

    Saving work

--- src/prefect/castor2sqlite.py
+++ src/prefect/castor2sqlite.py
@@ -1,34 +1,24 @@
 import os
 import logging
 
-from datetime import datetime
 from prefect import flow, task
-from barbell2.castor.castor2sqlite import CastorToSqlite
 
 STUDY_NAME = 'ESPRESSO_v2.0_DPCA'
 CLIENT_ID = open(os.path.join(os.environ['HOME'], 'castorclientid.txt'), 'r').readline().strip()
 CLIENT_SECRET = open(os.path.join(os.environ['HOME'], 'castorclientsecret.txt'), 'r').readline().strip()
-
 LOGGER = logging.getLogger(__name__)
+
 logging.basicConfig(level=logging.INFO)
+
 
 @task(name='extract_data')
 def extract_data():
-    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-    converter = CastorToSqlite(
-        study_name=STUDY_NAME,
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        output_db_file=f'/tmp/castor_{timestamp}.db',
-        cache=True,
-        record_offset=0,
-        max_nr_records=1,
-        log_level=logging.INFO,
-    )
-    converter.execute()
+    pass
+
 
 @flow(name='castor2sqlite')
 def castor2sqlite():
     extract_data()
+
 
 castor2sqlite()
