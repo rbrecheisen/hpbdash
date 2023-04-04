7266331485cd67e7477ae52f49d64ecd42d48c0b
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2023-04-04 13:16:44 +0200

    Saving work

--- airflow/docker-init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p ./dags ./logs ./plugins ./db
-echo -e "AIRFLOW_UID=$(id -u)" > .env  # IMPORTANT! Without this environment variable, it doesn't work!
-echo -e "CASTOR_CLIENT_ID=$(cat $HOME/castorclientid.txt)" >> .env
-echo -e "CASTOR_CLIENT_SECRET=$(cat $HOME/castorclientsecret.txt)" >> .env
-docker-compose up airflow-init
