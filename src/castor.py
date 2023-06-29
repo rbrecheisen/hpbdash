a38711b0c433601f4fdafc921b79d46585874edb
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2023-06-29 16:47:45 +0200

    saving work

--- src/castor.py
+++ src/castor.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 
 from barbell2_castor.castor2sqlite import CastorToDict
 
@@ -16,15 +17,35 @@ class CastorToJson:
         data = castor2dict.execute()
         with open(self.output_json, 'w') as f:
             json.dump(data, f)
+        return self.output_json
 
 
 class CastorJsonToDataFrame:
+
+    def __init__(self, input_json):
+        self.input_json = input_json
+    
+    def execute(self):
+        with open(self.input_json, 'r') as f:
+            data = json.load(f)
+        df_data = {}
+        for field_name in data.keys():
+            df_data[field_name] = data[field_name]['field_values']
+        df = pd.DataFrame(data=df_data)
+        for field_name in data.keys():
+            if data[field_name]['field_type'] == 'date':
+                df[field_name] = pd.to_datetime(df[field_name], dayfirst=True, errors='coerce')
+        return df
+    
+
+class CastorDataFrameQueryBuilder:
     pass
 
 
-class CastorDataFrameQuery:
-    pass
+class CastorDataFrameQueryRunner:
 
-
-class CastorQueryResult:
-    pass
+    def __init__(self, df):
+        self.df = df
+    
+    def execute(self, query):
+        return self.df.query(query)
